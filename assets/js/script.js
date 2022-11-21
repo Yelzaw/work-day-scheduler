722325e2e5a11b6c9f9d87dea47e03f2b96c0734
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2022-11-21 19:55:09 -0400

    Time Label Store as Int

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,13 +21,20 @@ $(function () {
     var saveTime = $(this).parent().attr("id");
     localStorage.setItem(saveTime,saveText);
   });
-  //
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  function compareTime() {
+    $(".time-block").each(function(i, obj){
+        var timeNote = parseInt($(this).attr("id").split("hour-")[1]);
+        console.log(timeNote);
+    });
+    }
+    compareTime()
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
